6bc2c14afa5d020d554218a5b688b656a5abf9b3
unknown <daniel@danielstern.ca>
2014-03-05 10:32:59 -0500

    working simulation, working shape api, much WIN

--- src/js/Rectangular.js
+++ src/js/Rectangular.js
@@ -9,6 +9,7 @@ angular.module('Rectangular')
 	 	 world.CreateBody(definition.b)
 		 .CreateFixture(definition.f);
 	 }
+
 	 this.setWorld = function(gravityX, gravityY, sleep) {
 	 		console.log("setting world", canvas);
 		 	var gravity = new b2Vec2(gravityX, gravityY);
@@ -22,8 +23,8 @@ angular.module('Rectangular')
 	 this.getWorld = function() {
 	 	return world;
 	 }
-	 
 })
+
 .service('ngrLoop', function(){
 	var l = this;
 	var ctx = {};
@@ -46,6 +47,7 @@ angular.module('Rectangular')
 		loop = setInterval(l.tick, 1000 / speed)
 	}
 })
+
 .service('ngrDebug',function(ngWorld){
 	this.debug = function(ctx) {
 
@@ -63,131 +65,124 @@ angular.module('Rectangular')
 })
 
 .service("ngBox",function(ngWorld){
-	 this.getBox = function(x, y, width, height, options) {
 
-		 //default setting
-		     var options = {
-		         'density' : 1.0 ,
-		         'friction' : 0.2 ,
-		         'restitution' : 0.2 ,
-		          
-		         'linearDamping' : 0.0 ,
-		         'angularDamping' : 0.0 ,
-		          
-		         'gravityScale' : 1.0 ,
-		         'type' : b2Body.b2_dynamicBody
-		     };
+	this.shape = function(type, options) {
 
-		     var world = ngWorld.getWorld();
+		//default options
+		var defaults = {
+			height: 2,
+			width: 2,
+			x: 10,
+			y: 1,
+			radius: 1,
+			density : 1.0 ,
+			'friction' : 0.2 ,
+			'restitution' : 0.6 ,
+			'linearDamping' : 0.0 ,
+			'angularDamping' : 0.0 ,
+			gravityScale : 1.0 ,
+			position : 'dynamic' ,
+			angle: 0,
+		};
 
-		     var body_def = new b2BodyDef();
-		     var fix_def = new b2FixtureDef;
-		      
-		     fix_def.density = options.density;
-		     fix_def.friction = options.friction;
-		     fix_def.restitution = options.restitution;
-		      
-		     fix_def.shape = new b2PolygonShape();
-		      
-		     fix_def.shape.SetAsBox( width , height );
-		      
-		     body_def.position.Set(x , y);
-		      
-		     body_def.linearDamping = options.linearDamping;
-		     body_def.angularDamping = options.angularDamping;
-		      
-		     body_def.type = options.type;
+		options = _.extend(defaults,options);
 
-		     return {b:body_def,f:fix_def};
+		switch(type) {
+			case 'box':
+			case 'rectangle':
+			case 'square':
+				options.type = 'box';
+				break;
+			case 'circle':
+			case 'ellipse':
+				options.type = 'circle';
+				break;
+			default:
+			console.log("Can't do that");
+			return;
+		}
 
+		function NgShape(options) {
+			var _shape = this;
 
-	 };
+			_shape.b = new b2BodyDef();
+   		_shape.f = new b2FixtureDef;
 
-	  this.getBall = function(x,y,radius) {
+   		var fix_def = _shape.f;
+   		var body_def = _shape.b;
 
-	 	 //default setting
-	 	     var options = {
-	 	         'density' : 1.0 ,
-	 	         'friction' : 0.2 ,
-	 	         'restitution' : 0.2 ,
-	 	          
-	 	         'linearDamping' : 0.0 ,
-	 	         'angularDamping' : 0.0 ,
-	 	          
-	 	         'gravityScale' : 1.0 ,
-	 	         'type' : b2Body.b2_dynamicBody
-	 	     };
+   		if (options.type == 'box') {
+   			fix_def.shape = new b2PolygonShape();
+   			fix_def.shape.SetAsBox( options.width , options.height );
+   		}
 
-	 	     var world = ngWorld.getWorld();
+   		if (options.type == 'circle') {
+   			fix_def.shape = new b2CircleShape();
+   			fix_def.shape.SetRadius( options.radius );
+   		}
 
-	 	     var body_def = new b2BodyDef();
-	 	     var fix_def = new b2FixtureDef;
-	 	      
-	 	     fix_def.density = options.density;
-	 	     fix_def.friction = options.friction;
-	 	     fix_def.restitution = options.restitution;
-	 	      
-	 	     fix_def.shape = new b2CircleShape();
-	 	      
-	 	     fix_def.shape.SetRadius( radius );
-	 	      
-	 	     body_def.position.Set(x , y);
-	 	      
-	 	     body_def.linearDamping = options.linearDamping;
-	 	     body_def.angularDamping = options.angularDamping;
-	 	      
-	 	     body_def.type = options.type;
+   		console.log("Options?", options)
 
-	 	     return {b:body_def,f:fix_def};
+   		body_def.position.Set(options.x , options.y);
 
 
-	  };
+   		fix_def.density = options.density;
+   		fix_def.friction = options.friction;
+   		fix_def.restitution = options.restitution;
+
+
+   		if (options.position == 'dynamic') {
+   			body_def.type = b2Body.b2_dynamicBody;
+   		}
+
+   		body_def.angle = options.angle;
+
+
+   		body_def.linearDamping = options.linearDamping;
+   		body_def.angularDamping = options.angularDamping;
+		}
+
+		var s = new NgShape(options);
+
+
+		return s;
+	}
+
 
 	 this.getFloor = function() {
-	 	 	var SCALE = 30;
+	 	 	var SCALE = ngWorld.scale;
 		 	   
  	     var fixDef = new b2FixtureDef;
- 	     fixDef.density = 1.0;
- 	     fixDef.friction = 0.5;
- 	     fixDef.restitution = 0.2;
- 	   
  	     var bodyDef = new b2BodyDef;
  	   
- 	     //create ground
  	     bodyDef.type = b2Body.b2_staticBody;
- 	     
- 	     // positions the center of the object (not upper left!)
+ 	    
  	     bodyDef.position.x = 300 / 2 / SCALE;
  	     bodyDef.position.y = 350 / SCALE;
-
- 	     bodyDef.angle = 0.01*Math.PI;
  	     
  	     fixDef.shape = new b2PolygonShape;
- 	     
- 	     // half width, half height. eg actual height here is 1 unit
+ 	    
  	     fixDef.shape.SetAsBox((600 / SCALE) / 2, (10/SCALE) / 2);
- 	   //  world.CreateBody(bodyDef).CreateFixture(fixDef);
 
- 	   return {b:bodyDef, f:fixDef};
+ 	 	  return {b:bodyDef, f:fixDef};
 	 }
 })
-.directive('ngStage',function(ngWorld, ngBox, ngrDebug, ngrLoop){
-	return {
-		restrict: 'AE',
-		link: function(scope, elem, attrs) {
-			console.log("New stage",elem, attrs)
-			// debugging block
-		}
-	}
-})
+
 .directive('ngBox',function(ngWorld, ngBox){
 	return {
 		restrict: 'AE',
 		link: function(scope, elem, attrs) {
-			console.log("New stage",elem, attrs);
-			 var box = ngBox.getBox(5, 5, 2,2);
-    		ngWorld.addElement(box);
-			// debugging block
+  		var box = ngBox.shape("box")
+  		ngWorld.addElement(box);
+		}
+	}
+})
+.directive('ngCircle',function(ngWorld, ngBox){
+	return {
+		restrict: 'AE',
+		link: function(scope, elem, attrs) {
+  		var circle = ngBox.shape("ellipse");
+  		ngWorld.addElement(circle);
 		}
 	}
 })
