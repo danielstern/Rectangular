c64ffa6b4a2e12c97160962dbe04054bb32171bb
unknown <daniel@danielstern.ca>
2014-03-09 19:38:14 -0400

    added better music support for ngaudio

--- src/lib/angular.audio.js
+++ src/lib/angular.audio.js
@@ -24,9 +24,14 @@ angular.module('ngAudio',[])
 	var a = this;
 	var mutedSounds = [];
 	var soundVolumes = {};
+	var songs = [];
 
 	this.play = function(id) {
 		var $sound = document.getElementById(id);
+
+		if (_.contains(songs,id)){
+			a.stop(songs);
+		};
 
 		/* Play the sound. */
 		$sound.pause();
@@ -35,15 +40,22 @@ angular.module('ngAudio',[])
 
 	};
 
+this.isMusic = function(ids) {
+	if (!_.isArray(ids)) {
+		ids = [ids];
+	}
+	_.each(ids,function(id){
+		songs.push(id);
+	})
+}
+
 	this.mute = function(ids) {
 		if (!_.isArray(ids)) {
 			ids = [ids];
 		}
 		_.each(ids,function(id){
-			//console.log("Iterating...",id);
 			mutedSounds.push(id);
 			soundVolumes[id] = a.getSoundVolume(id);
-			//console.log("Sound volumes?",a.getSoundVolume(id), soundVolumes)
 			a.setSoundVolume(id, 0);
 		})
 	};
@@ -54,7 +66,7 @@ angular.module('ngAudio',[])
 		};
 
 		_.each(ids,function(id){
-			console.log("Toggling mute",mutedSounds,id);
+		//	console.log("Toggling mute",mutedSounds,id);
 			if (_.contains(mutedSounds, id)) {
 				a.unmute(id);
 			} else {
@@ -64,7 +76,6 @@ angular.module('ngAudio',[])
 	}
 
 	this.unmute = function(id) {
-		//console.log("Unmuting");
 		mutedSounds = _.without(mutedSounds, id);
 		a.setSoundVolume(id, soundVolumes[id]);
 	};
@@ -81,12 +92,19 @@ angular.module('ngAudio',[])
 		$sound.volume = vol;
 	}
 
-	this.stop = function(id) {
-		var $sound = document.getElementById(id);
+	this.stop = function(ids) {
+		if (!_.isArray(ids)) {
+			ids = [ids];
+		};
 
-		/* stop the sound. */
-		$sound.pause();
-		$sound.currentTime = 0;
+		_.each(ids,function(id){
+
+			var $sound = document.getElementById(id);
+
+			/* stop the sound. */
+			$sound.pause();
+			$sound.currentTime = 0;
+		});
 
 	};
 
