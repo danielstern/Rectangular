fcc5409004adba175b87277357204660c1ca5762
unknown <daniel@danielstern.ca>
2014-03-13 19:44:29 -0400

    joints working

--- src/js/util.js
+++ src/js/util.js
@@ -1,42 +1,66 @@
 function MouseTargeter(_canvas, scale) {
 
-    function getMousePos(canvas, evt) {
-        var rect = canvas.getBoundingClientRect();
-        return {
-          x: evt.clientX - rect.left,
-          y: evt.clientY - rect.top
-        };
-    }
+  function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+      x: evt.clientX - rect.left,
+      y: evt.clientY - rect.top
+    };
+  }
 
-    var canvas = _canvas;
-    var context;
-    var SCALE = scale;
+  var canvas = _canvas;
+  var context;
+  var SCALE = scale;
 
-    var onmoveListeners = [];
+  var onmoveListeners = [];
+  var onclicklisteners = [];
 
-    this.onmove = function(listener) {
-        onmoveListeners.push(listener);
-    }
- 
-    context = canvas.getContext('2d');
-    canvas.addEventListener('mousemove', function(evt) {
-        var r = {}
-        var mousePos = getMousePos(canvas, evt);
-       // var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
-       // console.log(canvas, message);
-        r.worldPosX = mousePos.x / SCALE;
-        r.worldPosY = mousePos.y / SCALE;
-        r.mousePosX = mousePos.x;
-        r.mousePosY = mousePos.y;
-//        console.log(mousePos.x, mousePos.y, "-", worldPosX, worldPosY);
-        //console.log(r);
+  this.onmove = function(listener) {
+    onmoveListeners.push(listener);
+  }
 
-        _.each(onmoveListeners,function(_listener){
-            _listener(r);
-        })
+  this.onclick = function(listener) {
+    onclicklisteners.push(listener);
+  }
+
+  context = canvas.getContext('2d');
+  canvas.addEventListener('mousemove', function(evt) {
+    var r = {}
+    var mousePos = getMousePos(canvas, evt);
+    // var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
+    // console.log(canvas, message);
+    r.worldPosX = mousePos.x / SCALE;
+    r.worldPosY = mousePos.y / SCALE;
+    r.mousePosX = mousePos.x;
+    r.mousePosY = mousePos.y;
+    //        console.log(mousePos.x, mousePos.y, "-", worldPosX, worldPosY);
+    //console.log(r);
+
+    _.each(onmoveListeners, function(_listener) {
+      _listener(r);
+    })
 
 
-    }, false);
+  }, false);
+
+  canvas.addEventListener('click', function(evt) {
+    var r = {}
+    var mousePos = getMousePos(canvas, evt);
+    // var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
+    // console.log(canvas, message);
+    r.worldPosX = mousePos.x / SCALE;
+    r.worldPosY = mousePos.y / SCALE;
+    r.mousePosX = mousePos.x;
+    r.mousePosY = mousePos.y;
+    //        console.log(mousePos.x, mousePos.y, "-", worldPosX, worldPosY);
+    //console.log(r);
+
+    _.each(onclicklisteners, function(_listener) {
+      _listener(r);
+    })
+
+
+  }, false);
 
 
 }
@@ -59,42 +83,41 @@ function MouseTargeter(_canvas, scale) {
  */
 /* global Mousetrap:true */
 Mousetrap = (function(Mousetrap) {
-    var self = Mousetrap,
-        _oldBind = self.bind,
-        args;
+  var self = Mousetrap,
+    _oldBind = self.bind,
+    args;
 
-    self.bind = function() {
-        args = arguments;
+  self.bind = function() {
+    args = arguments;
 
-        // normal call
-        if (typeof args[0] == 'string' || args[0] instanceof Array) {
-            return _oldBind(args[0], args[1], args[2]);
-        }
+    // normal call
+    if (typeof args[0] == 'string' || args[0] instanceof Array) {
+      return _oldBind(args[0], args[1], args[2]);
+    }
 
-        // object passed in
-        for (var key in args[0]) {
-            if (args[0].hasOwnProperty(key)) {
-                _oldBind(key, args[0][key], args[1]);
-            }
-        }
-    };
+    // object passed in
+    for (var key in args[0]) {
+      if (args[0].hasOwnProperty(key)) {
+        _oldBind(key, args[0][key], args[1]);
+      }
+    }
+  };
 
-    return self;
-}) (Mousetrap);
+  return self;
+})(Mousetrap);
 
-var b2Vec2 = Box2D.Common.Math.b2Vec2
-    ,   b2AABB = Box2D.Collision.b2AABB
-    ,   b2BodyDef = Box2D.Dynamics.b2BodyDef
-    ,   b2Body = Box2D.Dynamics.b2Body
-    ,   b2FixtureDef = Box2D.Dynamics.b2FixtureDef
-    ,   b2Fixture = Box2D.Dynamics.b2Fixture
-    ,   b2World = Box2D.Dynamics.b2World
-    ,   b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape
-    ,   b2CircleShape = Box2D.Collision.Shapes.b2CircleShape
-    ,   b2DebugDraw = Box2D.Dynamics.b2DebugDraw
-    ,   b2MouseJointDef =  Box2D.Dynamics.Joints.b2MouseJointDef
-    ,   b2Shape = Box2D.Collision.Shapes.b2Shape
-    ,   b2RevoluteJointDef = Box2D.Dynamics.Joints.b2RevoluteJointDef
-    ,   b2Joint = Box2D.Dynamics.Joints.b2Joint
-    ,   b2PrismaticJointDef = Box2D.Dynamics.Joints.b2PrismaticJointDef
-    ;
+var b2Vec2 = Box2D.Common.Math.b2Vec2,
+  b2AABB = Box2D.Collision.b2AABB,
+  b2BodyDef = Box2D.Dynamics.b2BodyDef,
+  b2Body = Box2D.Dynamics.b2Body,
+  b2FixtureDef = Box2D.Dynamics.b2FixtureDef,
+  b2Fixture = Box2D.Dynamics.b2Fixture,
+  b2World = Box2D.Dynamics.b2World,
+  b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape,
+  b2CircleShape = Box2D.Collision.Shapes.b2CircleShape,
+  b2DebugDraw = Box2D.Dynamics.b2DebugDraw,
+  b2MouseJointDef = Box2D.Dynamics.Joints.b2MouseJointDef,
+  b2Shape = Box2D.Collision.Shapes.b2Shape,
+  b2RevoluteJointDef = Box2D.Dynamics.Joints.b2RevoluteJointDef,
+  b2Joint = Box2D.Dynamics.Joints.b2Joint,
+  b2PrismaticJointDef = Box2D.Dynamics.Joints.b2PrismaticJointDef;
--- src/shapemaker/shapemaker.js
+++ src/shapemaker/shapemaker.js
@@ -20,36 +20,55 @@
      };
 
      var targeter = new MouseTargeter($('canvas')[0],ngrState.getScale());
-     targeter.onmove(function(r){
+     targeter.onclick(function(r){
       //  console.log("mouse is over the canvas", r)      ;
-         $scope.r = r;
-         $scope.$apply();
+      var body = getBodyAtMouse(r);
+      console.log("Body?",body,r);
 
-         var targetVec = {x:r.worldPosX,y:r.worldPosY};
-         var pVec = new b2Vec2(targetVec.x,targetVec.y);
-         var aabb = new b2AABB();
-         aabb.lowerBound.Set(targetVec.x - 0.001, targetVec.y - 0.001);
-         aabb.upperBound.Set(targetVec.x + 0.001, targetVec.y + 0.001);
-
-         var targetBody = null;
-         var Fixture;
-
-         function GetBodyCallback(Fixture) {
-            var shape = Fixture.GetShape();
-            var Inside = shape.TestPoint(Fixture.GetBody().GetTransform(),pVec);
-            if (Inside) {
-               targetBody = Fixture.GetBody();
-               return false;
-            }
-            return true;
-         }
-
-         ngrWorld.getWorld().QueryAABB(GetBodyCallback, aabb);
-         console.log('target?',targetBody);
-         return targetBody;
-
+      if (body) {
+            var m_world = ngrWorld.getWorld();
+            var mouse_joint = new b2MouseJointDef();
+            var pixels_in_a_meter = ngrState.getScale();
+            var mouseX = r.mousePosX;
+            var mouseY = r.mousePosY;
+            mouse_joint.bodyA = m_world.GetGroundBody();
+            mouse_joint.bodyB = body;
+            mouse_joint.target.Set(r.worldPosX, r.worldPosY);
+            mouse_joint.maxForce = 10000;
+            //mouse_joint.timeStep = 1 / 60;
+            console.log("Mouse joint?",mouse_joint);
+            mouseJoint = m_world.CreateJoint(mouse_joint);
+        }
 
      })
+
+     function getBodyAtMouse(r) {
+      $scope.r = r;
+      $scope.$apply();
+
+      var targetVec = {x:r.worldPosX,y:r.worldPosY};
+      var pVec = new b2Vec2(targetVec.x,targetVec.y);
+      var aabb = new b2AABB();
+      aabb.lowerBound.Set(targetVec.x - 0.001, targetVec.y - 0.001);
+      aabb.upperBound.Set(targetVec.x + 0.001, targetVec.y + 0.001);
+
+      var targetBody = null;
+      var Fixture;
+
+      function GetBodyCallback(Fixture) {
+         var shape = Fixture.GetShape();
+         var Inside = shape.TestPoint(Fixture.GetBody().GetTransform(),pVec);
+         if (Inside) {
+            targetBody = Fixture.GetBody();
+            return false;
+         }
+         return true;
+      }
+
+      ngrWorld.getWorld().QueryAABB(GetBodyCallback, aabb);
+      //console.log('target?',targetBody);
+      return targetBody;
+     }
 
    })
    .directive('shapemaker', function() {
