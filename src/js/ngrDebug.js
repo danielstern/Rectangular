d70ba2b7b0c891cd4cfbdf2bea21b629cb336505
unknown <daniel@danielstern.ca>
2014-03-13 19:08:17 -0400

    added more info to slider

--- src/js/ngrDebug.js
+++ src/js/ngrDebug.js
@@ -18,16 +18,5 @@ angular.module('Rectangular')
     debugDraw.SetFlags(b2DebugDraw.e_shapeBit | b2DebugDraw.e_jointBit);
     world.SetDebugDraw(debugDraw);
 
-    p = setInterval(function(){
-      var state = ngrState.getState();
-      //var currentTransform = ctx.currentTransform;
-      //
-      //var current 
-      //console.log("Current debug transform?",currentTransform);
-       // var newScale = ngrState.getScale();
-        //if (newScale != oldScale) d.debugDraw.SetDrawScale(newScale);
-
-        
-    },1)
   }
 })
