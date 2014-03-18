4bc14375b000b2c4fed78cfe70209b945112cc27
unknown <daniel@danielstern.ca>
2014-03-17 23:31:38 -0400

    working on getting ball agent working

--- src/BallAgent/BallAgent.js
+++ src/BallAgent/BallAgent.js
@@ -1,241 +1,251 @@
 angular.module("BallAgent", ['Rectangular', 'ngAudio', 'BallAgentHero', 'BallAgentModels'])
   .service('BallAgent', function(BallAgentLevels, BallAgentHero, BallAgentModels, ngAudio, ngrEnvironment, ngrDisplay, ngrBox, ngrWorld) {
 
-    this.state = {};
-    var state = this.state;
-    var stateChangeListeners = [];
-    var gameOverListeners = [];
-    var winListeners = [];
-    var airborneTimer = 3;
-    var jumpReleasedTimer = 15;
-    var LIVES = 2;
-    var m = BallAgentModels;
-    var hero,
-      exit,
-      controls,
-      jumpReleased = true;
+      this.state = {};
+      var state = this.state;
+      var stateChangeListeners = [];
+      var gameOverListeners = [];
+      var winListeners = [];
+      var airborneTimer = 3;
+      var jumpReleasedTimer = 15;
+      var LIVES = 2;
+      var m = BallAgentModels;
+      var hero,
+        exit,
+        controls,
+        jumpReleased = true;
 
-    this.onStateChange = function(listener) {
-      stateChangeListeners.push(listener);
-    }
+      this.onStateChange = function(listener) {
+        stateChangeListeners.push(listener);
+      }
 
-    function updateState() {
-      _.each(stateChangeListeners, function(l) {
-        l(state);
-      })
-    }
+      function updateState() {
+        _.each(stateChangeListeners, function(l) {
+          l(state);
+        })
+      }
 
-    this.onGameOver = function(listener) {
-      gameOverListeners.push(listener);
-    }
+      this.onGameOver = function(listener) {
+        gameOverListeners.push(listener);
+      }
 
-    this.onWin = function(listener) {
-      winListeners.push(listener);
-    }
+      this.onWin = function(listener) {
+        winListeners.push(listener);
+      }
 
-    this.gotoLevel = function(level) {
-      state.currentLevel = level - 1;
-      nextLevel();
-    }
-    
-
-    function bindControls() {
-
-      var heroState = BallAgentHero.getState();
-
-      Mousetrap.bind({
-        'd': function() {
-          heroState.goingRight = true;
-        },
-        'a': function() {
-          heroState.goingLeft = true;
-        },
-        'w': function() {
-          if (!jumpReleased) return;
-          heroState.isJumping = true;
-          jumpReleasedTimer = 15;
-          jumpReleased = false;
-        },
-      }, 'keydown');
-
-      Mousetrap.bind({
-        'd': function() {
-          heroState.goingRight = false;
-        },
-        'a': function() {
-          heroState.goingLeft = false;
-        },
-        'w': function() {
-          heroState.isJumping = false;
-          jumpReleased = true;
-        },
-      }, 'keyup');
-
-    }
-
-    function handleDeath() {
-      state.lives--;
-
-      if (state.lives >= 0) {
-        state.currentLevel--;
+      this.gotoLevel = function(level) {
+        state.currentLevel = level - 1;
         nextLevel();
-      } else {
-        gameOver();
-      }
-    }
-
-    function gameOver() {
-      ngrEnvironment.stop();
-      _.each(gameOverListeners, function(l) {
-        l(state);
-      });
-
-      ngrEnvironment.blocker()
-      .then(function(){
-        ngrEnvironment.clearAll();
-      })
-    }
-
-    function win() {
-      ngrEnvironment.stop();
-      _.each(winListeners, function(l) {
-        l(state);
-      });
-
-      ngrEnvironment.blocker()
-      .then(function(){
-        ngrEnvironment.clearAll();
-      })
-    }
-
-
-
-    function newGame() {
-      state.lives = LIVES;
-      state.currentLevel = 0;
-      nextLevel();
-    }
-
-    this.newGame = newGame;
-
-    function tick() {
-
-      var heroState = BallAgentHero.getState(),
-        position = BallAgentHero.entity.GetPosition(),
-        contacts = BallAgentHero.entity.GetContactList();
-
-      if (airborneTimer) airborneTimer--;
-      if (jumpReleasedTimer) jumpReleasedTimer--;
-
-      if (!airborneTimer) {
-        heroState.airborne = true;
       }
 
-      if (contacts && contacts.contact) {
-        while (contacts) {
-          var contact = contacts.contact;
+      ngAudio.muteAll();
 
-          if (contact.IsTouching() && contacts.other.GetUserData()) {
-            var data = contacts.other.GetUserData();
 
-            if (data.exit) {
-              ngAudio.play('exit');
-              nextLevel();
-            }
-          }
+      function bindControls() {
 
-          if (contact.IsTouching() && contacts.other.GetUserData() && contacts.other.GetUserData().isFloor) {
-            if (!jumpReleasedTimer) {
-              jumpReleased = true;
-              if (!airborneTimer) ngAudio.play('land');
-            }
+        var heroState = BallAgentHero.getState();
 
-            heroState.airborne = false;
-            airborneTimer = 3;
+        Mousetrap.bind({
+          'd': function() {
+            heroState.goingRight = true;
+          },
+          'a': function() {
+            heroState.goingLeft = true;
+          },
+          'w': function() {
+            if (!jumpReleased) return;
+            heroState.isJumping = true;
+            jumpReleasedTimer = 15;
+            jumpReleased = false;
+          },
+        }, 'keydown');
 
-          }
+        Mousetrap.bind({
+          'd': function() {
+            heroState.goingRight = false;
+          },
+          'a': function() {
+            heroState.goingLeft = false;
+          },
+          'w': function() {
+            heroState.isJumping = false;
+            jumpReleased = true;
+          },
+        }, 'keyup');
 
-          contacts = contacts.next;
+      }
+
+      function handleDeath() {
+        state.lives--;
+
+        if (state.lives >= 0) {
+          state.currentLevel--;
+          nextLevel();
+        } else {
+          gameOver();
         }
       }
 
+      function gameOver() {
+        ngrEnvironment.stop();
+        _.each(gameOverListeners, function(l) {
+          l(state);
+        });
 
-      if (position.y > 50) {
-        ngAudio.play('die');
-        handleDeath();
+        ngrEnvironment.blocker()
+          .then(function() {
+            ngrEnvironment.clearAll();
+          })
+      }
+
+      function win() {
+        ngrEnvironment.stop();
+        _.each(winListeners, function(l) {
+          l(state);
+        });
+
+        ngrEnvironment.blocker()
+          .then(function() {
+            ngrEnvironment.clearAll();
+          })
       }
 
 
-    }
 
-    var c;
-    var goingToNextLevel = false;
+      function newGame() {
+        state.lives = LIVES;
+        state.currentLevel = 0;
+        nextLevel();
+      }
 
-    ngrEnvironment.init({
-      floor: false,
-      gravity: 30,
-      scale: 30,
-      worldWidth: 25,
-    });
-    function nextLevel() {
-      if (goingToNextLevel) return;
-      goingToNextLevel = true;
+      this.newGame = newGame;
 
-      ngrEnvironment.stop();
-      ngrEnvironment.blocker()
-        .then(function() {
-         // console.log("Creating new level");
-          state.currentLevel++;
-          var l = BallAgentLevels.levels[state.currentLevel - 1];
+      function tick() {
 
-          if (!l) {
-             win();
-             return;
+        var heroState = BallAgentHero.getState(),
+          position = BallAgentHero.entity.GetPosition(),
+          contacts = BallAgentHero.entity.GetContactList();
+
+        if (airborneTimer) airborneTimer--;
+        if (jumpReleasedTimer) jumpReleasedTimer--;
+
+        if (!airborneTimer) {
+          heroState.airborne = true;
+        }
+
+        if (contacts && contacts.contact) {
+          while (contacts) {
+            var contact = contacts.contact;
+
+            if (contact.IsTouching() && contacts.other.GetUserData()) {
+              var data = contacts.other.GetUserData();
+
+              if (data.exit) {
+                ngAudio.play('exit');
+                nextLevel();
+              }
+            }
+
+            if (contact.IsTouching() && contacts.other.GetUserData() && contacts.other.GetUserData().isFloor) {
+              if (!jumpReleasedTimer) {
+                jumpReleased = true;
+                if (!airborneTimer) ngAudio.play('land');
+              }
+
+              heroState.airborne = false;
+              airborneTimer = 3;
+
+            }
+
+            contacts = contacts.next;
           }
-
-          ngrEnvironment.clearAll();
-          if (c) clearTimeout(c);
-
-          state.levelName = l.levelName;
-
-          $('.levelName').removeClass('slideInLeft')
-            .removeClass('slideOutLeft')
-          setTimeout(function(){
-             $('.levelName').addClass('animated slideInLeft');
-            }, 1);
-
-          c = setTimeout(function(){
-             $('.levelName').addClass('animated slideOutLeft');
-          }, 3000);
-
-          ngrEnvironment.start();
-
-          if (l.floor) ngrEnvironment.floor({
-            src: 'img/tile-blue.png',
-            bg: 'tiled',
-          });
-          if (l.lWall) ngrEnvironment.leftWall();
-          if (l.rWall) ngrEnvironment.rightWall();
-
-        //  ngrEnvironment.debug();
-          ngrDisplay.background(l.background || 'img/mountain-bg.jpg');
-
-          hero = BallAgentHero.createNewHero();
-          exit = m.createExit(l.exit);
-          bindControls();
-
-          goingToNextLevel = false;
-
-          _.each(l.columns, m.createColumn);
-          _.each(l.platforms, m.createPlatform);
-
-          ngrEnvironment.addHook(tick);
-
-          updateState(state);
-
-        });
+        }
 
 
-    }
+        if (position.y > 50) {
+          ngAudio.play('die');
+          handleDeath();
+        }
 
-  })
+
+      }
+
+      var c;
+      var goingToNextLevel = false;
+
+      ngrEnvironment.init({
+          scale: 30,
+          floor: true,
+          worldHeight: 30,
+          zoom:1,
+          room: {
+            width: 90,
+            height: 40,
+            floor: true,
+            leftWall: true,
+            rightWall: true,
+            roof: false
+          }});
+
+        function nextLevel() {
+          if (goingToNextLevel) return;
+          goingToNextLevel = true;
+
+          ngrEnvironment.stop();
+          ngrEnvironment.blocker()
+            .then(function() {
+              state.currentLevel++;
+              var l = BallAgentLevels.levels[state.currentLevel - 1];
+
+              if (!l) {
+                win();
+                return;
+              }
+
+              ngrEnvironment.clearAll();
+              if (c) clearTimeout(c);
+
+              state.levelName = l.levelName;
+
+              $('.levelName').removeClass('slideInLeft')
+                .removeClass('slideOutLeft')
+              setTimeout(function() {
+                $('.levelName').addClass('animated slideInLeft');
+              }, 1);
+
+              c = setTimeout(function() {
+                $('.levelName').addClass('animated slideOutLeft');
+              }, 3000);
+
+              ngrEnvironment.start();
+
+              if (l.floor) ngrEnvironment.floor({
+                src: 'img/tile-blue.png',
+                bg: 'tiled',
+              });
+              if (l.lWall) ngrEnvironment.leftWall();
+              if (l.rWall) ngrEnvironment.rightWall();
+
+              //  ngrEnvironment.debug();
+              ngrDisplay.background(l.background || 'img/mountain-bg.jpg');
+
+
+              hero = BallAgentHero.createNewHero();
+              exit = m.createExit(l.exit);
+              bindControls();
+
+              goingToNextLevel = false;
+
+              _.each(l.columns, m.createColumn);
+              _.each(l.platforms, m.createPlatform);
+
+              ngrEnvironment.addHook(tick);
+
+              updateState(state);
+
+            });
+
+
+        }
+
+      })
