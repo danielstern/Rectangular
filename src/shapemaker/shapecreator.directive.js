8a8938f895e937ff3c824bac997ac536a855c6c9
unknown <daniel@danielstern.ca>
2014-04-04 11:18:34 -0400

    got shapes adding again...

--- src/shapemaker/shapecreator.directive.js
+++ src/shapemaker/shapecreator.directive.js
@@ -11,56 +11,57 @@ angular.module('shapemaker')
       controller: function ($scope, $attrs, $element, ngrEnvironment, ngrWorld, ngrState, shapemakerDefaults) {
 
         $scope.defaults = shapemakerDefaults.shapeDefaultParams;
-        $scope.preset = {};
-        var paramaters = $scope.paramaters = {};
+
+        $scope.shapes = shapemakerDefaults.shapeOptions;
+        $scope.skins = shapemakerDefaults.skins;
+        $scope.presets = shapemakerDefaults.presets;
+        $scope.preset = _.sample($scope.presets);
+        $scope.params = {};
 
         $scope.$watchCollection('preset', function () {
           var pre = $scope.preset;
           if (!pre.shape) return;
           console.log("Preset?", pre);
           _.each(pre.presets, function (_pre, key) {
-            $scope.paramaters[key] = _pre;
+            $scope.params[key] = _pre;
           });
 
-          $scope.paramaters.userData = $scope.preset.userData || {};
+          $scope.params.userData = $scope.params.userData || {};
 
-          $scope.paramaters.shape = _.find($scope.options, function (option) {
+          $scope.params.shape = _.find($scope.shapes, function (option) {
             return (pre.shape == option.type);
           });
 
-          $scope.paramaters.skin = _.find($scope.skins, function (_skin) {
+          $scope.params.skin = _.find($scope.skins, function (_skin) {
             return (_skin.src === pre.skin.src);
           });
 
-          $scope.paramaters.shapeKind = paramaters.shape.type;
+          $scope.params.shapeKind = $scope.params.shape.type;
 
-          if (pre.skin) paramaters.src = pre.skin.src;
+          if (pre.skin) $scope.params.src = pre.skin.src;
         })
 
         $scope.newShape = function (input) {
           $scope.addShape();
         }
 
-        $scope.$watch('paramaters', function () {
-          if (!$scope.paramaters.shape) return;
-          console.log("paramaters watch time", $scope.paramaters.shape)
-          $scope.defaults[$scope.paramaters.shape.type].params.split(' ');
+        $scope.$watch('params', function () {
+          if (!$scope.params.shape) return;
+          $scope.defaults[$scope.params.shape.type].params.split(' ');
         }, true)
 
-        $scope.options = shapemakerDefaults.shapeOptions;
-        $scope.skins = shapemakerDefaults.skins;
-        $scope.presets = shapemakerDefaults.presets;
-        $scope.paramaters.preset = _.sample($scope.presets);
 
         $scope.addShape = function (shape) {
 
-          if (paramaters.preset.skin) _.extend(paramaters, paramaters.preset.skin);
-          if (paramaters.customSrc) {
-            paramaters.src = $scope.paramaters.customSrc;
+          var preset = $scope.preset;
+
+          if (preset.skin) _.extend($scope.params, preset.skin);
+          if ($scope.params.customSrc) {
+            $scope.params.src = $scope.$scope.params.customSrc;
           };
-          paramaters.profile = paramaters.preset.profile;
-          console.log("adding shape,", shape, paramaters.preset);
-          ngrWorld.addElement(shapemakerDefaults.shape(paramaters));
+          $scope.params.profile = preset.profile;
+          console.log("adding shape,", shape, preset);
+          ngrWorld.addElement(shapemakerDefaults.shape($scope.params));
         }
 
         $scope.destroy = function () {
