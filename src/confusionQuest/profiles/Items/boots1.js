7917f441b188de01fa9cefb771e87618e3676263
unknown <daniel@danielstern.ca>
2014-04-15 12:45:06 -0400

    seperate item class works

--- src/confusionQuest/profiles/Items/boots1.js
+++ src/confusionQuest/profiles/Items/boots1.js
@@ -1,51 +1,66 @@
 angular.module('ConfusionQuest')
 
-  .service('boots1', function (ngrGame,ConfusionQuestDefaults) {
-    var stats = {
-      id: "boots1",
-      img: 'img/powershoe.png',
-      name: "Trevon's Greaves of Dunking",
-      description: "These legendary shoes increase your jumping power by 10%. Also makes you run a bit faster.",
-      flavor: "Thou tries to dunks't against me, ser?",
-      hero: {
-        speed: 5,
-        jump: 10
-      }
+.service('boots1', function (ngrGame, ConfusionQuestDefaults, Item) {
+  var stats = {
+    id: "boots1",
+    img: 'img/powershoe.png',
+    name: "Trevon's Greaves of Dunking",
+    description: "These legendary shoes increase your jumping power by 10%. Also makes you run a bit faster.",
+    flavor: "Thou tries to dunks't against me, ser?",
+    hero: {
+      speed: 5,
+      jump: 10
     }
-    var Boots1 = function (body) {
-      body.onimpact(function (other) {
+  }
 
-        if (other.GetUserData() && other.GetUserData().isHero) {
-          body.crumble();
-          console.log("Power up!");
-          ngrGame.powerup(stats);
+  var Boots1 = function (body) {
+
+    _.extend(this, new Item.profile);
+    this.stats = stats;
+    this.init(body);
+
+  };
+
+  var defaults = {
+    name: 'Powerup - Boots 1',
+    shape: 'box',
+    skin: {
+      src: 'img/powershoe.png',
+      bg: 'sprite'
+    },
+    profile: "boots1",
+    presets: Item.dimensions()
+  };
+
+  ConfusionQuestDefaults.addDefault(defaults);
+  ngrGame.addProfile('boots1', Boots1);
+
+})
+  .service("Item", function (ngrGame) {
+    this.profile = function () {
+      return {
+        init: function (body) {
+          var item = this;
+          item.body = body || this.body;
+          item.body.onimpact(function (other) {
+
+            if (other.GetUserData() && other.GetUserData().isHero) {
+              item.body.crumble();
+              console.log("powerup...",item);
+              ngrGame.powerup(item.stats);
+            }
+          })
         }
-      })
+      }
     };
 
-    var defaults = {
-      name: 'Powerup - Boots 1',
-      shape: 'box',
-      skin: {
-        src: 'img/powershoe.png',
-        bg: 'sprite'
-      },
-      profile: "boots1",
-      userData: {
-        doodad: true,
-        isCoin: true,
-      },
-      presets: {
+    this.dimensions = function () {
+      return {
         radius: 1,
         height: 3,
         width: 3,
         restitution: 0.2,
         density: 0.2
       }
-
-    };
-
-    ConfusionQuestDefaults.addDefault(defaults);
-    ngrGame.addProfile('boots1', Boots1);
-
+    }
   })
