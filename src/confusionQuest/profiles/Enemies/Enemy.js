c9d102fc1d6ac385a39d1a73cbb4c11d75cab15a
unknown <daniel@danielstern.ca>
2014-04-16 12:44:43 -0400

    prototyping is stuuuupid

--- src/confusionQuest/profiles/Enemies/Enemy.js
+++ src/confusionQuest/profiles/Enemies/Enemy.js
@@ -8,63 +8,28 @@ angular.module('ConfusionQuest')
     function newEnemy(stats) {
 
       var Enemy = Entity(stats);
-
-
-
-      return Enemy;
-
-    }
-
-    return Enemy;
-  })
-.service("Entity",function(ngrGame, ngrLoop, ngrWorld, ConfusionQuestSFX, ConfusionQuestDefaults){
-
-  function Entity(stats) {
-    return newEntity(stats);
-  }
-
-  function newEntity(stats) {
-
-    var Entity = function (body) {
-      
-      this.stats = stats;
-
-      var entity = this;
-      entity.body = body;
-      body.profile = this;
-  
-      this.state = {
-        facingLeft: true,
-        facingRight: false,
-        isJumping: false,
-        isAttacking: false,
-        hp: this.stats.health,
-      }
-
-      this.getState = function () {
-        return entity.state;
-      };
-
-      this.die =  function () {
+      Enemy.prototype.die = function () {
         var pos = this.body.GetPosition();
         ngrGame.effect(ConfusionQuestSFX.explosion3Big, pos);
         this.body.crumble();
         this.state.dead = true;
       };
 
-      this.damage = function (dmg) {
+      Enemy.prototype.damage = function (dmg) {
         this.state.hp -= dmg;
-      }
-      
-      this.init =  function () {
+      };
 
-        this.state.hp = entity.stats.health;
+      Enemy.prototype.init = function () {
+        
+        var enemy = this;
 
-        entity.body.onimpact(function (other) {
+        this.state.hp = enemy.stats.health;
+
+        enemy.body.onimpact(function (other) {
 
           if (other.GetUserData() && other.GetUserData().isHero) {
             var hero = other.profile;
-            hero.damage(entity.stats.attack, entity);
+            hero.damage(enemy.stats.attack, enemy);
           }
         });
 
@@ -72,50 +37,79 @@ angular.module('ConfusionQuest')
 
         ngrLoop.addHook(function () {
 
-          if (entity.state.dead) return;
-          if (entity.body.sprite && entity.body.sprite.animation) {
-            var anim = entity.body.sprite.animation;
+          if (enemy.state.dead) return;
+          if (enemy.body.sprite && enemy.body.sprite.animation) {
+            var anim = enemy.body.sprite.animation;
 
             _.each(anim.spriteSheet.getAnimations(), function (animation) {
               anim.spriteSheet.getAnimation(animation).speed = 0.4;
             });
           };
 
-          if (entity.state.hp <= 0) {
+          if (enemy.state.hp <= 0) {
 
-            entity.die();
+            enemy.die();
           }
 
         })
 
-
-
       }
-        this.init();
 
-      entity.tick = function () {
-        if (entity.state.isAttacking) {
-          console.log("entity attack!");
+      return Enemy;
+
+    }
+
+    return Enemy;
+  })
+  .service("Entity", function (ngrGame, ngrLoop, ngrWorld, ConfusionQuestSFX, ConfusionQuestDefaults) {
+
+    function Entity(stats) {
+      return newEntity(stats);
+    }
+
+    function newEntity(stats) {
+
+      var Entity = function (body) {
+
+        this.stats = stats;
+
+        var entity = this;
+        entity.body = body;
+        body.profile = this;
+
+        this.state = {
+          facingLeft: true,
+          facingRight: false,
+          isJumping: false,
+          isAttacking: false,
+          hp: this.stats.health,
         }
 
-      }
+        this.getState = function () {
+          return entity.state;
+        };
 
-      ngrGame.control(entity, {
-        'a': 'goingLeft',
-        'd': 'goingRight',
-        'w': 'isJumping',
-        'p': 'isAttacking',
-      })
-    };
-    
-    
+        this.init();
+
+        entity.tick = function () {
+          if (entity.state.isAttacking) {
+            console.log("entity attack!");
+          }
+
+        }
+
+        ngrGame.control(entity, {
+          'a': 'goingLeft',
+          'd': 'goingRight',
+          'w': 'isJumping',
+          'p': 'isAttacking',
+        })
+      };
+
+      return Entity;
+
+    }
+
     return Entity;
 
-    
-   
-  }
-  
-   return Entity;
-
-
-})
+  })
